e7fe23c592b76e3e57e20055a5a5f7cbf9407c8f
sfK <sfk.sjm@gmail.com>
2024-01-07 20:17:56 +0900

    first commit

---
+++ Activity Indicators/Activity_IndicatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Activity_IndicatorsApp.swift
+//  Activity Indicators
+//
+//  Created by 성준모 on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Activity_IndicatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Activity Indicators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Activity Indicators
+//
+//  Created by 성준모 on 1/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
